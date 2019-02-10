c2507b74a6da768ee246d50d8f4022366a469e8a
Maksym Davydov <mdavydov@adva-soft.com>
2019-02-10 14:30:38 +0200

    Initial commit

---
+++ Lab1iOS/Lab1iOS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Lab1iOS
+//
+//  Created by Maksym Davydov on 2/10/19.
+//  Copyright © 2019 LPNU. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Lab1iOS/Lab1iOS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Lab1iOS
+//
+//  Created by Maksym Davydov on 2/10/19.
+//  Copyright © 2019 LPNU. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
